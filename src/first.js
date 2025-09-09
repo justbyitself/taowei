7df74313623a12f89786a1bbd33273b143225ed9
justbyitself <justbyitself@proton.me>
2025-09-09 13:48:45 +0200

    fix(first): remove Iterator.from

--- src/first.js
+++ src/first.js
@@ -1,7 +1,7 @@
 import concat from './concat.js'
 
 export default (...args) => {
-  const it = Iterator.from(concat(...args))
+  const it = concat(...args)
   const res = it.next()
   return res.done ? undefined : res.value
 }
