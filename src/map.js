b23807f5684296ecf5ef2a22dc68423a9503a65c
justbyitself <justbyitself@proton.me>
2025-09-09 07:07:26 +0200

    fix(map): replace let by const

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ import concat from './concat.js'
 
 export default fn => (...args) => function* () {
   const iterable = concat(...args)
-  for (let value of iterable) {
+  for (const value of iterable) {
     yield fn(value)
   }
 }()
