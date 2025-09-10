ddb35742a56381ab3ac22ccdca0c1d48043817bb
justbyitself <justbyitself@proton.me>
2025-09-10 10:20:26 +0200

    fix(zip): minor test fixes

--- test/unit/zip.test.js
+++ test/unit/zip.test.js
@@ -1,13 +1,13 @@
 import { describe, it, expect } from 'vitest'
 import zip from '../../src/zip.js'
 
-describe('zip function', () => {
-  it('should zip iterables of the same length', () => {
+describe('zip', () => {
+  it('combines iterables of the same length', () => {
     const result = zip([1,2,3], ['a','b','c'])
     expect([...result]).toEqual([[1,'a'], [2,'b'], [3,'c']])
   })
 
-  it('should limit to the shortest iterable', () => {
+  it('limits to the shortest iterable', () => {
     const result = zip([1,2,3], ['a','b'])
     expect([...result]).toEqual([[1,'a'], [2,'b']])
   })
