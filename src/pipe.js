dc7d1836e73c8d736ed353ecaeff911a6ea317b2
justbyitself <justbyitself@proton.me>
2025-09-10 10:16:15 +0200

    fix(pipe): use foldWithInit

--- src/pipe.js
+++ src/pipe.js
@@ -1,8 +1,7 @@
 import compose from './compose.js'
-import uncurry from './uncurry.js'
-import fold from './fold.js'
+import foldWithInit from './foldWithInit.js'
 import id from './id.js'
 import flip from './flip.js'
 
-export default fold(uncurry(flip(compose)))(id)
+export default foldWithInit(flip(compose))(id)
 
