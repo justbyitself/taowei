a95704902a06c528c259b6c51dd9e7fe82da9367
justbyitself <justbyitself@proton.me>
2025-09-12 11:53:40 +0200

    test: removed old tests

--- test/integration/fibonacci.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import { withPrevious, take, repeat, sum, toArray, flat } from '../../src'
-
-describe('Fibonacci sequence', () => {
-  it('takes first 10 numbers', () => {
-    const fibonacci = withPrevious(2)(flat([0, 1, repeat(sum)]))
-    expect(toArray(take(10)(fibonacci))).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
-  })
-})
