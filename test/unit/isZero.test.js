5caa89b77b591b84dee0f986af123c530e879714
justbyitself <justbyitself@proton.me>
2025-09-12 18:14:14 +0200

    test(unit): fix imports in isZero.test.js

--- test/unit/isZero.test.js
+++ test/unit/isZero.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { isZero } from '../../src'
+import { isZero } from '../../src/index.js'
 
 describe('isZero', () => {
   it('true only for numeric 0 (=== 0)', () => {
