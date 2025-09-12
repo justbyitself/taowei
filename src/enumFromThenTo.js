6aa44842c506b52a7a1469101ae9b12484bb21af
justbyitself <justbyitself@proton.me>
2025-09-12 18:14:13 +0200

    chore(src): apply lint fixes to enumFromThenTo.js

--- src/enumFromThenTo.js
+++ src/enumFromThenTo.js
@@ -1,13 +1,5 @@
 import enumFromThen  from './enumFromThen.js'
 import takeWhile from './takeWhile.js'
-import lte from './isLesserThanOrEqualTo.js'
-import gte from './isGreaterThanOrEqualTo.js'
-import ifThenElse from './ifThenElse.js'
-
-/*
-export default from => then => to => takeWhile((ifThenElse(gte(then)(from))(lte)(gte))(to))
-  (enumFromThen(from)(then))
-*/
 
 export default from => then => to => {
   const step = then - from
