255f7d25a28576cf5cf25f2b52ba5ecc5f72f176
justbyitself <justbyitself@proton.me>
2025-09-12 18:19:48 +0200

    refactor(words): simpler implementation

--- src/words.js
+++ src/words.js
@@ -1,16 +1,3 @@
 export default str => function* () {
-  str = str.trim()
-
-  const regex = /\s+/g
-  let startIndex = 0
-  let match
-
-  while ((match = regex.exec(str)) !== null) {
-    yield str.slice(startIndex, match.index)
-    startIndex = match.index + match[0].length
-  }
-
-  if (startIndex < str.length) {
-    yield str.slice(startIndex)
-  }
+  yield* str.trim().split(/\s+/).filter(Boolean)
 }()
