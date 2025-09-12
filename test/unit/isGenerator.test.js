86613f32298eb8c359fea38d74a6fd0a68ab7ef2
justbyitself <justbyitself@proton.me>
2025-09-12 18:14:14 +0200

    test(unit): fix imports in isGenerator.test.js

--- test/unit/isGenerator.test.js
+++ test/unit/isGenerator.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import isGenerator from '../../src/isGenerator'
+import isGenerator from '../../src/isGenerator.js'
 
 function* sampleGen() { yield 1 }
 
