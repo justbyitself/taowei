f8914f889339c83e8f57192aa50f3b36635a2987
justbyitself <justbyitself@proton.me>
2025-09-12 18:14:13 +0200

    test(unit): fix imports in isFunction.test.js

--- test/unit/isFunction.test.js
+++ test/unit/isFunction.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { isFunction } from '../../src'
+import { isFunction } from '../../src/index.js'
 
 describe('isFunction', () => {
   it('should return true for functions', () => {
