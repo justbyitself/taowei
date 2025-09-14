7eceecd6f41b8fc1150982424270d90e49d8b503
justbyitself <justbyitself@proton.me>
2025-09-14 07:05:55 +0200

    refactor: update flatten import

--- src/zipOf.js
+++ src/zipOf.js
@@ -1,7 +1,7 @@
 import zip from './zip.js'
 import fold from './fold.js'
 import map from './map.js'
-import flat from './flat.js'
+import flat from './flattenDeep.js'
 import toArray from './toArray.js'
 import compose from './compose.js'
 
