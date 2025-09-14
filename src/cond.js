93076b040fff48ee94c4d8b208fd1ad84e680084
justbyitself <justbyitself@proton.me>
2025-09-14 22:48:14 +0200

    refactor(cond)

--- src/cond.js
+++ src/cond.js
@@ -1,15 +1,13 @@
 import toMap from './toMap.js'
-import isFunction from './isFunction.js'
-import isEqualTo from './isEqualTo.js'
+import toFunction from './toFunction.js'
+import toPredicate from './toPredicate.js'
 
-export default (...predicates) => v => {
-  const map = toMap(...predicates)
-  const toFunction = v => isFunction(v) ? v : isEqualTo(v)
+export default predicates => {
+  const map = toMap(predicates)
 
-  for (const [key, value] of map) {
-    const predicate = toFunction(key)
-    if (predicate(v)) {
-      return value
+  return value => {
+    for (const [predicate, transformer] of map) {
+      if (toPredicate(predicate)(value)) return toFunction(transformer)(value)
     }
   }
 }
