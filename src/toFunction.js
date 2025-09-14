6092de0221cc63f3e4ab5602aa8d474a2f5270fb
justbyitself <justbyitself@proton.me>
2025-09-14 22:41:43 +0200

    refactor(toFunction): tiny refactoring

--- src/toFunction.js
+++ src/toFunction.js
@@ -1,5 +1,4 @@
 import constant from './constant.js'
-import ifThenElse from './ifThenElse.js'
 import isFunction from './isFunction.js'
 
-export default x => ifThenElse(isFunction(x))(x)(constant(x))
+export default x => isFunction(x) ? x : constant(x)
