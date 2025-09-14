09a0cd2f2d169da33da837447711bef2b3a999e7
justbyitself <justbyitself@proton.me>
2025-09-14 07:49:10 +0200

    fix: replace flattenDeep by flatten()

--- src/zipOf.js
+++ src/zipOf.js
@@ -1,8 +1,8 @@
 import zip from './zip.js'
 import fold from './fold.js'
 import map from './map.js'
-import flat from './flattenDeep.js'
+import flatten from './flatten.js'
 import toArray from './toArray.js'
 import compose from './compose.js'
 
-export default iterable => map(compose(toArray)(flat))(fold(zip)(iterable))
+export default iterable => map(compose(toArray)(flatten()))(fold(zip)(iterable))
