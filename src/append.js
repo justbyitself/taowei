bf166771e86659a9f3df29dda4596b356e2cb927
justbyitself <justbyitself@proton.me>
2025-09-26 14:50:54 +0200

    feat(append): function overriding

--- src/append.js
+++ src/append.js
@@ -1,1 +1,16 @@
-export default post => text => `${text}${post}`
+import isArray from './isArray.js'
+import isMap from './isMap.js'
+import isPojo from './isPojo.js'
+import cond from './cond.js'
+import isSet from './isSet.js'
+import appendValuesArray from './appendValuesArray.js'
+import appendValuesSet from './appendValuesSet.js'
+import appendEntriesMap from './appendEntriesMap.js'
+import appendEntriesObject from './appendEntriesObject.js'
+
+export default value => cond([
+  [isArray, appendValuesArray(value)],
+  [isSet,   appendValuesSet(value)],
+  [isMap,   appendEntriesMap(value)],
+  [isPojo,  appendEntriesObject(value)],
+])
